7e00d779950e2faa1ec463ca8f89fca687a29379
Syed Fasih <elite9145build@gmail.com>
2025-08-17 15:52:13 +0300

    Added debugging route at /api

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+
 import devicesRoutes from "./routes/devices.js";
 import simRoutes from "./routes/sim.js";
 import deviceTypeRoutes from "./routes/deviceTypes.js";
+import helloRoutes from "./routes/hello.js";
 
 const app = express();
 app.use(cors());
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use("/api/devices", devicesRoutes);
 app.use("/api/sim", simRoutes);
 app.use("/api/devicetype", deviceTypeRoutes);
+app.use("/api", helloRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
